4dc175372f967d42b86f68705fd5d3bfa474fffb
GhostX-ai <egamovkhurshed29@gmail.com>
2021-03-04 16:06:34 +0500

    Print Hello Paymona!

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace GitLesson
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello Paymona!");
         }
     }
 }
